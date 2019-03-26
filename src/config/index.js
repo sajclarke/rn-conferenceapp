0edaa4278acbdd94e424e979b30a247f1bb3e7b2
Shannon Clarke <shannonajclarke@gmail.com>
2019-03-26 15:31:26 -0400

    Update about screen

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ export default {
   // API_URL: "https://api.myjson.com/bins/gen0e",
   API_URL:
     "https://raw.githubusercontent.com/sajclarke/rn-conferenceapp/master/",
+  // "https://raw.githubusercontent.com/sajclarke/rn-conferenceapp/blob/cc7570c5e1d2166cf6bf00bf4d3576fffdacfc1b/",
   PRIMARY_BG_COLOR: "#353535",
   PRIMARY_TEXT_COLOR: "#999999",
   SECONDARY_BG_COLOR: "#F5FCFF",
--- src/screens/AboutScreen/AboutView.js
+++ src/screens/AboutScreen/AboutView.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {
-  ScrollView, View, Text, Image
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
-import styles from './style.js';
-import config from '../../config';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { ScrollView, View, Text, Image } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import styles from "./style.js";
+import config from "../../config";
 
 class AboutView extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      conference: {},
+      conference: {}
     };
   }
 
@@ -35,36 +32,48 @@ class AboutView extends Component {
     return (
       <ScrollView>
         <Image
-          source={{uri: this.props.conference.banner}}
+          source={{ uri: this.props.conference.banner }}
           style={styles.banner}
         />
         <View style={styles.container}>
           <Text style={styles.title}>{this.props.conference.name}</Text>
           <View style={styles.info}>
-            <Icon.Button name="calendar" backgroundColor="transparent" color={config.PRIMARY_BG_COLOR}>
+            <Icon.Button
+              name="calendar"
+              backgroundColor="transparent"
+              color={config.PRIMARY_BG_COLOR}
+            >
               <Text>{this.props.conference.date}</Text>
             </Icon.Button>
-            <Icon.Button name="map-pin" backgroundColor="transparent" color={config.PRIMARY_BG_COLOR}>
-              <Text>{(this.props.conference.location||{}).city}</Text>
+            <Icon.Button
+              name="map-pin"
+              backgroundColor="transparent"
+              color={config.PRIMARY_BG_COLOR}
+            >
+              <Text>{(this.props.conference.location || {}).city}</Text>
             </Icon.Button>
           </View>
-          <Text style={styles.description}>{this.props.conference.description}</Text>
+          <Text style={styles.description}>
+            {this.props.conference.description}
+          </Text>
           <View style={styles.author}>
             <Image
-              source={{uri: 'https://avatars3.githubusercontent.com/u/17863319?s=460&v=4'}}
+              source={{
+                uri:
+                  "https://avatars3.githubusercontent.com/u/17863319?s=460&v=4"
+              }}
               style={styles.avatar}
             />
-            <View>
+            {/* <View>
               <Text style={styles.name}>App Developer Info:</Text>
               <Text>Lex Martinez</Text>
               <Text style={styles.link}>me@lexmartinez.com</Text>
-            </View>
+            </View> */}
           </View>
         </View>
       </ScrollView>
     );
   }
-
 }
 
 AboutView.propTypes = {
